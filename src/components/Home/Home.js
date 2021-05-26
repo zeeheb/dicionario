9fca691c73b0a83b0f765a9b96c9b1b2106b72d4
José Eduardo Brandão <zefutbrandao@gmail.com>
2021-05-26 16:19:35 -0300

    query ajustado mas nao final

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import '../../styles/home.css'
 import AddIcon from '@material-ui/icons/Add';
-
+import SearchIcon from '@material-ui/icons/Search';
 
 export class Home extends Component {
     render() {
@@ -11,18 +11,24 @@ export class Home extends Component {
                     <span className="titleHome">Dicionário Colaborativo de Libras</span>
                     <button className="cadastrarBtn">Cadastre uma palavra <AddIcon/></button>
                 </div>
-                {/*<div className="query">*/}
-                {/*    <span>Palavra</span>*/}
-                {/*    <div className="searchWord">*/}
-                {/*        <input type="text" placeholder="Busca por palavra..."/>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
+
                 <div className="shadow">
                 <div className="qry">
-                    <span>Palavra</span>
-                        <div className="searchWord">
-                            <input type="text" placeholder="Busca por palavra..."/>
-                        </div>
+                    <div className="palavra">
+                        <span>Por Palavra</span>
+                        <input type="text" placeholder="Busca por palavra..."/>
+                        <SearchIcon className="searchicon"/>
+                    </div>
+                    <div className="alfabetica">
+                        <span>Por ordem alfabética</span>
+                        <input type="text" placeholder="Busca por palavra..."/>
+                        <SearchIcon className="searchicon"/>
+                    </div>
+                    <div className="mao">
+                        <span>Por configuração de mão</span>
+                        <input type="text" placeholder="Busca por palavra..."/>
+                        <SearchIcon className="searchicon"/>
+                    </div>
                 </div>
                 </div>
             </div>
