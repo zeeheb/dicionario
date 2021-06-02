9fcfb7d9cf9bf0d9648601da10097cf5303133d7
José Eduardo Brandão <zefutbrandao@gmail.com>
2021-06-01 21:47:24 -0300

    basic form css html

--- src/components/Cadastro/Cadastrar.js
+++ src/components/Cadastro/Cadastrar.js
@@ -1,11 +1,12 @@
-import React, {Component} from "react";
+import React, {Component, useState} from "react";
 import '../../styles/cadastrar.css'
 import { useHistory } from 'react-router-dom'
-import AddIcon from "@material-ui/icons/Add";
-import SearchIcon from "@material-ui/icons/Search";
+
 
 function Cadastrar(props) {
     const history = useHistory();
+    const[palavra, setPalavra] = useState('');
+    const[regiao, setRegiao] = useState('');
 
     const backBtn = () => {
         history.push('/');
@@ -16,7 +17,7 @@ function Cadastrar(props) {
 
         <div className="superContainer">
             <div className="navigation">
-                <span className="titleHome">Dicionário Colaborativo de Libras</span>
+                <span onClick={backBtn} className="titleHome">Dicionário Colaborativo de Libras</span>
             </div>
 
             <div className="shadow">
@@ -25,7 +26,22 @@ function Cadastrar(props) {
                 </div>
             </div>
         <div className="form">
+            <div className="input">
+                <label htmlFor="palavra">Palavra</label>
+                <input name="palavra" type="text" onChange={(event) => {
+                    setPalavra(event.target.value);
+                }}/>
+            </div>
+            <div className="input">
+                <label htmlFor="regiao">Região</label>
+                <input name="regiao" type="text" onChange={(event) => {
+                    setRegiao(event.target.value);
+                }}/>
+            </div>
 
+            <div>
+                <button className="cadastrarBtn2">Submeter</button>
+            </div>
         </div>
         </div>
     );
