3100ce2f7a709440a2368543d36c01aa3f9fa17b
José Eduardo Brandão <zefutbrandao@gmail.com>
2021-06-21 22:54:27 -0300

    correções modal config e nova tela login

--- src/components/Cadastro/ModalConfigMaos.js
+++ src/components/Cadastro/ModalConfigMaos.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from "react";
+import React, {Component, useEffect, useState} from "react";
 import {configMaos} from "./config-maos";
 import '../../styles/modal-config.css'
 import 'react-toastify/dist/ReactToastify.css';
@@ -8,13 +8,17 @@ import {Button} from "@material-ui/core";
 
 function ModalConfigMaos(props) {
 
-
+useEffect(() => {
+    console.log(configMaos);
+})
     return (
-        <div className="conteiner">
+        <div className="flex flex-wrap gap-15">
             {
                 configMaos.map(config => (
-                        <div className="config-item">
-                            <img src={config.url} alt={config.value} onClick={(e) => props.onClickConfig(e.target.alt)}/>
+                        <div>
+                            <img
+                                className=" border-2 border-solid border-white hover:border-indigo-500 cursor-pointer"
+                                src={config.url} alt={config.value} onClick={(e) => props.onClickConfig(e.target.alt)}/>
                         </div>
 
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,45 +3,58 @@ import '../../styles/login.css';
 import '../../App.css';
 
 import 'react-toastify/dist/ReactToastify.css';
+import {useHistory} from "react-router-dom";
 
 
 
 function Login(props) {
-
+    const [register, setRegister ] = useState(true);
+    const history = useHistory();
+    const backBtn = () => {
+        history.push('/');
+    }
 
 
     return (
 
-        <div className="loginContainer">
-            <div
-                className="max-w-lg max-w-xs bg-blue-800 shadow-2xl rounded-lg mx-auto text-center py-12 mt-4 rounded-xl">
-                <h1 className="text-gray-200 text-center font-extrabold -mt-3 text-3xl">Tailbox</h1>
-                <div className="container py-5 max-w-md mx-auto">
-                    <form method="" action="">
-                        <div className="mb-4">
-                            <input placeholder="Username"
-                                   className="shadow appearance-none  rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                   id="username" type="text"/>
-                        </div>
-                        <div className="mb-6">
+        <div
+            className="p-20 h-screen w-screen flex flex-col-reverse md:flex-row items-center justify-center bg-indigo-400">
+            <div className="container mx-auto flex flex-col items-center">
+                <button onClick={backBtn} className="hover:bg-gray-300 mb-3 bg-white text-black p-3 rounded-lg font-semibold text-lg">
+                     Voltar
+                </button>
+                <form className="shadow-lg w-80 p-4 flex flex-col bg-white rounded-lg">
+                    <input type="text" placeholder="Email"
+                           className="mb-3 py-3 px-4 border border-gray-400 focus:outline-none rounded-md focus:ring-1 ring-cyan-500"/>
+                    <input type="text" placeholder="Senha"
+                           className="mb-3 py-3 px-4 border border-gray-400 focus:outline-none rounded-md focus:ring-1 ring-cyan-500"/>
+                    {register &&
+                        <>
+                        <input type="text" placeholder="Confirmação de senha"
+                               className="mb-3 py-3 px-4 border border-gray-400 focus:outline-none rounded-md focus:ring-1 ring-cyan-500"/>
 
-                            <input placeholder="Password"
-                                   className="shadow appearance-none  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                                   id="password" type="password"/>
-
-                        </div>
-                        <div className="flex items-center justify-between">
+                        <input type="text" placeholder="Usuário"
+                               className="mb-3 py-3 px-4 border border-gray-400 focus:outline-none rounded-md focus:ring-1 ring-cyan-500"/>
+                    </>
+                    }
+                    { !register &&
+                        <>
                             <button
-                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                                type="button">
-                                Sign In
+                                className="w-full bg-blue-500 text-white p-3 rounded-lg font-semibold text-lg">Entrar
                             </button>
-                            <a className="inline-block align-baseline font-bold text-sm text-gray-400 " href="#">
-                                Forgot Password?
-                            </a>
-                        </div>
-                    </form>
-                </div>
+                            <a className="text-blue-400 text-center my-2">Esqueci minha senha</a>
+                            <hr/>
+                        </>
+                    }
+                    <button
+                        className="w-full bg-green-400 mt-8 mb-4 text-white p-3 rounded-lg font-semibold text-lg">
+                        Criar nova conta
+                    </button>
+                </form>
+                <p className="text-center text-sm my-4">
+                    <span className="font-semibold text-center w-full">Create a Page</span> for a celebrity, band or
+                    business
+                </p>
             </div>
         </div>
     );
