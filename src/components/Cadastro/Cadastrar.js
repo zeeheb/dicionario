82a2d28a363c48ded3370c9fc102bd4671d172bb
José Eduardo Brandão <zefutbrandao@gmail.com>
2021-06-30 20:00:34 -0300

    input de qtd de config de mao

--- src/components/Cadastro/Cadastrar.js
+++ src/components/Cadastro/Cadastrar.js
@@ -17,10 +17,20 @@ function Cadastrar(props) {
     const history = useHistory();
     const [palavra, setPalavra] = useState('');
     const [regiao, setRegiao] = useState('');
-    const [configMao, setConfigMao] = useState('');
-    const [classifMao, setClassifMao] = useState('');
-    const [pontoArtic, setPontoArtic] = useState('');
+    const [qtdConfig, setQtdConfig] = useState(1);
+    const [qtdPt, setQtdPt] = useState(1);
+    const [configMao1, setConfigMao1] = useState('');
+    const [configMao2, setConfigMao2] = useState('');
+    const [configMao3, setConfigMao3] = useState('');
+    const [configMao4, setConfigMao4] = useState('');
+    const [pontoArtic1, setPontoArtic1] = useState('');
+    const [pontoArtic2, setPontoArtic2] = useState('');
+    const [pontoArtic3, setPontoArtic3] = useState('');
+    const [pontoArtic4, setPontoArtic4] = useState('');
     const [show, setShow] = useState(false);
+    const [show2, setShow2] = useState(false);
+    const [show3, setShow3] = useState(false);
+    const [show4, setShow4] = useState(false);
 
 
     const backBtn = () => {
@@ -28,9 +38,9 @@ function Cadastrar(props) {
     }
 
     const addRegistro = () => {
-        console.log('palavra: ' +palavra, 'regiao: ' +regiao, 'config: ' +configMao, 'artic:'+pontoArtic);
+        console.log('palavra: ' +palavra, 'regiao: ' +regiao, 'config: ' +configMao1, 'artic:'+pontoArtic1);
 
-        if (palavra == '' || regiao == '' || configMao == '' || pontoArtic == '') {
+        if (palavra == '' || regiao == '' || configMao1 == '' || pontoArtic1 == '') {
             toast.error("Não deixe campos vazios!!");
             return;
         }
@@ -51,8 +61,32 @@ function Cadastrar(props) {
         setShow(true);
     }
 
+    const openModal2 = () => {
+        setShow2(true);
+    }
+
+    const openModal3 = () => {
+        setShow3(true);
+    }
+
+    const openModal4 = () => {
+        setShow4(true);
+    }
+
     const handleClose = () => {
         setShow(false);
+    }
+
+    const handleClose2 = () => {
+        setShow2(false);
+    }
+
+    const handleClose3 = () => {
+        setShow3(false);
+    }
+
+    const handleClose4 = () => {
+        setShow4(false);
     }
 
     return (
@@ -113,7 +147,7 @@ function Cadastrar(props) {
                 <label htmlFor="articulacao">Ponte de Articulação <span className="red-star">*</span></label>
                 <div className="input">
                     <select name="articulacao" type="text" onChange={(event) => {
-                        setPontoArtic(event.target.value);
+                        setPontoArtic1(event.target.value);
                     }}>
                         <option disabled selected value></option>
                         <option value="1">Cabeça</option>
@@ -124,15 +158,58 @@ function Cadastrar(props) {
                 </div>
 
                 <label htmlFor="config">Configuração de Mão <span className="red-star">*</span></label>
+                <h3>Quantidade</h3>
+                <select onChange={(e) => {
+                    setQtdConfig(e.target.value);
+                }}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                </select>
                 <div className="input">
+
                     <div className="box-input-config">
                         <span className="box-choice" onClick={openModal}>
-                            <img src={configMao != '' ? `./config-maos/${configMao}.jpg` : ''}/>
+                            <img src={configMao1 != '' ? `./config-maos/${configMao1}.jpg` : ''}/>
                         </span>
                         <span className="box-icon" onClick={openModal}>
                          <AddIcon/>
                         </span>
                     </div>
+                    {
+                        qtdConfig >= "2" &&
+                        <div className="box-input-config">
+                        <span className="box-choice" onClick={openModal2}>
+                            <img src={configMao2 != '' ? `./config-maos/${configMao2}.jpg` : ''}/>
+                        </span>
+                            <span className="box-icon" onClick={openModal2}>
+                         <AddIcon/>
+                        </span>
+                        </div>
+                    }
+                    {
+                        qtdConfig >= "3" &&
+                        <div className="box-input-config">
+                        <span className="box-choice" onClick={openModal3}>
+                            <img src={configMao3 != '' ? `./config-maos/${configMao3}.jpg` : ''}/>
+                        </span>
+                            <span className="box-icon" onClick={openModal3}>
+                         <AddIcon/>
+                        </span>
+                        </div>
+                    }
+                    {
+                        qtdConfig === "4" &&
+                        <div className="box-input-config">
+                        <span className="box-choice" onClick={openModal4}>
+                            <img src={configMao4 != '' ? `./config-maos/${configMao4}.jpg` : ''}/>
+                        </span>
+                            <span className="box-icon" onClick={openModal4}>
+                         <AddIcon/>
+                        </span>
+                        </div>
+                    }
                 </div>
 
                 <label htmlFor="regiao">Imagem/Vídeo</label>
@@ -152,8 +229,55 @@ function Cadastrar(props) {
                     </DialogTitle>
                     <DialogContent>
                         <ModalConfigMaos onClickConfig={value => {
-                            setConfigMao(value);
+                            setConfigMao1(value);
                             setShow(false);
+                        }}/>
+                    </DialogContent>
+                </Dialog>
+
+                    <Dialog transitionDuration={{enter: 300, exit: 500}} maxWidth="md" onClose={handleClose2}
+                            aria-labelledby="simple-dialog-title" open={show2}>
+                        <DialogTitle id="simple-dialog-title">
+                            <div className="box-dialog" style={{textAlign: 'center'}}>
+                                <b style={{fontSize: '45px', color: '#696df6'}}>Escolha uma Configuração de Mão</b>
+                                <hr/>
+                            </div>
+                        </DialogTitle>
+                        <DialogContent>
+                            <ModalConfigMaos onClickConfig={value => {
+                                setConfigMao2(value);
+                                setShow2(false);
+                            }}/>
+                        </DialogContent>
+                    </Dialog>
+
+                <Dialog transitionDuration={{enter: 300, exit: 500}} maxWidth="md" onClose={handleClose3}
+                        aria-labelledby="simple-dialog-title" open={show3}>
+                    <DialogTitle id="simple-dialog-title">
+                        <div className="box-dialog" style={{textAlign: 'center'}}>
+                            <b style={{fontSize: '45px', color: '#696df6'}}>Escolha uma Configuração de Mão</b>
+                            <hr/>
+                        </div>
+                    </DialogTitle>
+                    <DialogContent>
+                        <ModalConfigMaos onClickConfig={value => {
+                            setConfigMao3(value);
+                            setShow3(false);
+                        }}/>
+                    </DialogContent>
+                </Dialog>
+                <Dialog transitionDuration={{enter: 300, exit: 500}} maxWidth="md" onClose={handleClose4}
+                        aria-labelledby="simple-dialog-title" open={show4}>
+                    <DialogTitle id="simple-dialog-title">
+                        <div className="box-dialog" style={{textAlign: 'center'}}>
+                            <b style={{fontSize: '45px', color: '#696df6'}}>Escolha uma Configuração de Mão</b>
+                            <hr/>
+                        </div>
+                    </DialogTitle>
+                    <DialogContent>
+                        <ModalConfigMaos onClickConfig={value => {
+                            setConfigMao4(value);
+                            setShow4(false);
                         }}/>
                     </DialogContent>
                 </Dialog>
